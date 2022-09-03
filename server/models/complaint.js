15bf6fc775d7e6164f39a965517bb50708bc2848
Random Name <abhinav.raja03@gmail.com>
2022-09-03 12:12:47 +0530

    Made initial versions of models for the server

---
+++ server/models/complaint.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const complaintSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    //To do: Make image actually work with images
+    image: {
+        type: String,
+        required: false
+    },
+    //Stores email of the complainant
+    complainant: {
+        type: String,
+        required: true
+    },
+    hostel: {
+        type: String, 
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Open',
+        enum: ['Open', 'In Progress', 'Resolved']
+    }
+})
---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    hostel: {
+        type: String,
+        required: true
+    },
+    //role controls the access level of the user
+    role: {
+        type: String,
+        required: true,
+        default: 'Student',
+        //Admin can view and update status of all complaints
+        enum: ['Student', 'Warden', 'Admin']
+    },
+});
